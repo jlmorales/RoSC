acd89fe4f88d2549690c9f0cf672d727055e5542
jlmorales <jose.l.morales@stonybrook.edu>
2019-03-28 15:47:01 -0400

    tutorial file and jupyter notebook

---
+++ cfg/app.py
@@ -0,0 +1,30 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+
+df = pd.read_csv('/Users/Jose/Documents/C4G-Abandoned-Call.csv')
+
+def generate_table(dataframe, max_rows=len(df)):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H4(children='Response of Suffolk County'),
+    generate_table(df)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
